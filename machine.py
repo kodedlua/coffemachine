af8b8a7632a1cb0cb7089f09797fd91ec9883bd6
kodedlua <kubawojt90@gmail.com>
2024-10-09 17:17:20 +0200

    Added adding and removing items from your cart, also cost is calculated when u added and remove items

--- machine.py
+++ machine.py
@@ -10,7 +10,7 @@ class Machine(object):
         self.stdscr = stdscr
         self.template, self.config = loader.load()
 
-    def render(self, height, width, current_rows, selected):
+    def render(self, height, width, current_rows, selected, balance):
         inputdata = self.config['inputdata']
         balance_cords = inputdata['balance']
         cost_cords = inputdata['cost']
@@ -21,6 +21,17 @@ class Machine(object):
         coffe_2_cost = convertCost(self.items[1][2])
         coffe_3_cords = inputdata['coffe-3']
         coffe_3_cost = convertCost(self.items[2][2])
+        dispenser_1_cords = inputdata['dispenser-1']
+        dispenser_1_count = selected.count(self.items[0][0])
+        dispenser_2_cords = inputdata['dispenser-2']
+        dispenser_2_count = selected.count(self.items[1][0])
+        dispenser_3_cords = inputdata['dispenser-3']
+        dispenser_3_count = selected.count(self.items[2][0])
+
+        cost = 0 + (self.items[0][2] * dispenser_1_count) + (self.items[1][2] * dispenser_2_count) + (self.items[2][2] * dispenser_3_count)
+        cost_text = "00.00"
+        if cost != 0:
+            cost_text = convertCost(cost)
 
         curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
         win = curses.newwin(height, width, 0, 0)
@@ -35,20 +46,20 @@ class Machine(object):
                         win.attron(curses.color_pair(1))
                     else:
                         win.attroff(curses.color_pair(1))
+                        
                     if (x == balance_cords['x'] and y == balance_cords['y']) or (x == balance_cords['x']+1 and y == balance_cords['y']) or (x == balance_cords['x']+2 and y == balance_cords['y']) or (x == balance_cords['x']+3 and y == balance_cords['y']) or (x == balance_cords['x']+4 and y == balance_cords['y']):
                         offset = x-balance_cords['x']
-                        if offset == 2:
-                            win.addstr(".")
-                        else:
-                            win.addstr("0")
+                        win.addstr(convertCost(balance)[offset])
                     elif (x == cost_cords['x'] and y == cost_cords['y']) or (x == cost_cords['x']+1 and y == cost_cords['y']) or (x == cost_cords['x']+2 and y == cost_cords['y']) or (x == cost_cords['x']+3 and y == cost_cords['y']) or (x == cost_cords['x']+4 and y == cost_cords['y']) or (x == cost_cords['x']+5 and y == cost_cords['y']):
                         offset = x-cost_cords['x']
                         if offset == 0:
                             win.addstr("-")
-                        elif offset == 3:
-                            win.addstr(".")
+                        # elif offset == 3:
+                        #     win.addstr(".")
+                        # else:
+                        #     win.addstr("0")
                         else:
-                            win.addstr("0")
+                            win.addstr(cost_text[offset-1])
                     elif (x == change_cords['x'] and y == change_cords['y']) or (x == change_cords['x']+1 and y == change_cords['y']) or (x == change_cords['x']+2 and y == change_cords['y']) or (x == change_cords['x']+3 and y == change_cords['y']) or (x == change_cords['x']+4 and y == change_cords['y']):
                         offset = x-cost_cords['x']
                         if offset == 3:
@@ -68,13 +79,19 @@ class Machine(object):
                         win.addstr(y, x, self.template[y][x])
                 except curses.error as e:
                     pass
+        if dispenser_1_count > 0:
+            win.addstr(dispenser_1_cords['y'], dispenser_1_cords['x'], f"x{dispenser_1_count} black")
+        if dispenser_2_count > 0:
+            win.addstr(dispenser_2_cords['y'], dispenser_2_cords['x'], f"x{dispenser_2_count} cappucino")
+        if dispenser_3_count > 0:
+            win.addstr(dispenser_3_cords['y'], dispenser_3_cords['x'], f"x{dispenser_3_count} latte")
         return win
 
     def renderHelpBox(self, height, maxwidth):
         curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLUE)
         helpBox = curses.newwin(5, maxwidth, height+4, 0)
         helpBox.attron(curses.color_pair(2))
-        helpBox.addstr("[UP KEY] [DOWN KEY] - navigate | [ENTER] - select | [Q] - exit")
+        helpBox.addstr("[UP KEY] [DOWN KEY] - navigate | [E] - select | [R] - remove | [Q] - exit")
         return helpBox
 
     def display(self):
@@ -92,7 +109,7 @@ class Machine(object):
         selected = []
 
         while True:
-            win = self.render(height, width, current_row, selected)
+            win = self.render(height, width, current_row, selected, 00.00)
             win.refresh()
             helpBox = self.renderHelpBox(height, cols)
             helpBox.refresh()
@@ -104,8 +121,13 @@ class Machine(object):
             elif event == curses.KEY_DOWN:
                 if current_row+1 <= 3:
                     current_row+=1
-            elif event == curses.KEY_ENTER:
+            elif event == ord("e"):
                 if current_row <= 2:
-                    selected.append(self.items[current_row][0])   
+                    if selected.count(self.items[current_row][0])+1 <= 9:
+                        selected.append(self.items[current_row][0])
+            elif event == ord("r"):
+                if current_row <= 2:
+                    if selected.count(self.items[current_row][0])-1 >= 0:
+                        selected.pop(selected.index(self.items[current_row][0]))
             elif event == ord("q"):
                 break
--- utils.py
+++ utils.py
@@ -1,5 +1,11 @@
-def convertCost(cost) -> str:
+def convertCost(cost: int) -> str:
     return "%02d" % (cost) + str('{0:.2f}'.format(float(cost%1))).removeprefix("0")
+
+def getValueIfExists(dictionary: dict, value: str):
+    if value in dictionary:
+        return dictionary[value]
+    else:
+        return 0
 
 STATE_SELECTED_1 = 1
 STATE_SELECTED_2 = 2
