141b025e907f2a8971a7e7765f7d30c67ce5a640
Koded <32173884+kodedlua@users.noreply.github.com>
2024-11-03 21:43:08 +0100

    Update utils.py
    Added notifications, centering print string and printing coffe as emojis

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import curses
+import winsound
+
 def formatNumber(cost: int) -> str:
     return "%02d" % (cost) + str('{0:.2f}'.format(float(cost%1))).removeprefix("0")
 
@@ -24,4 +27,27 @@ def moneyInNotesAsText(amount: int, notes: list=BANKNOTES) -> str:
                 for x in range(solution[i]): text.append(f"{banknote//100}$ ")
             else:
                 for x in range(solution[i]): text.append(f"{banknote}¢ ")
+    return "".join(text)
+
+def showNotification(self, title: str, text: str, nType: int):
+    if nType == 2:
+        winsound.PlaySound("SystemHand", winsound.SND_ASYNC)
+    else:
+        winsound.PlaySound("SystemQuestion", winsound.SND_ASYNC)
+    self.noti = [True, title, text, nType]
+
+def addCenteredString(window:curses.window, height: int, text: str):
+    _h, width = window.getmaxyx()
+    window.addstr(height, width//2-len(text)//2, text)
+
+def coffeeAsText(toDispense: list) -> str:
+    text = []
+
+    for i, coffee in enumerate(toDispense):
+        if coffee == "coffee-1":
+            text.append("🥤")
+        elif coffee == "coffee-2":
+            text.append("☕")
+        elif coffee == "coffee-3":
+            text.append("🧋")
     return "".join(text)
